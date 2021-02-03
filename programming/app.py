ffa0c824bb76473b23e2ac5cfc0a0f8375f18d94
Ahmed Moghaed <asaad0861@gmail.com>
2021-02-03 19:36:44 +0200

    fixed connecting app in container with compose

--- programming/app.py
+++ programming/app.py
@@ -8,13 +8,18 @@ import os
 
 # Init app
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-manager = Manager(app)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# manager = Manager(app)
 
 
 # DataBase
+
+# connection with database container with compose
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://devops:mypasswd@psql:5432/devops'
+
+# connection with local data base or remote db server
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/devops'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init DB
@@ -26,10 +31,13 @@ ma = Marshmallow(app)
 # IP Class/Model
 class IP(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ip = db.Column(db.String(15), unique=True)
+    ip = db.Column(db.String(15))
 
     def __init__(self,ip):
         self.ip = ip
+
+
+db.create_all()
 
 # IP Schema
 class IPSchema(ma.Schema):
@@ -51,7 +59,7 @@ def get():
         return jsonify({ "msg" : "{}".format(x) })
 
 
-@app.route('/ip', methods=['POST'])
+@app.route('/ip')
 def add_ip():
     request_ip = request.remote_addr
     new_ip = IP(ip=request_ip)
@@ -67,23 +75,20 @@ def get_allips():
 
 
 # manpulate server
-
-def create_db():
-    db.create_all()
-
-
-class CustomServer(Server):
-    def __call__(self, app, *args, **kwargs):
-        create_db()
-        return Server.__call__(self, app, *args, **kwargs)
-
-# Remeber to add the command to your Manager instance
-manager.add_command('runserver', CustomServer())
-
-if __name__ == "__main__":
-    manager.run()
-
-# # Run Server
-# if __name__ == '__main__':
-#     app.run(debug=True)
+# def create_db():
 #     db.create_all()
+
+# class CustomServer(Server):
+#     def __call__(self, app, *args, **kwargs):
+#         create_db()
+#         return Server.__call__(self, app, *args, **kwargs)
+
+# # Remeber to add the command to your Manager instance
+# manager.add_command('runserver', CustomServer())
+
+# if __name__ == "__main__":
+#     manager.run()
+
+# Run Server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050, debug=True)
