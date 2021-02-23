97b590aaa19c61870f607efe443f031cbb578252
AhmedMojahed <asaad0861@gmail.com>
2021-02-23 22:08:35 +0200

    Initial commit in Azure

---
+++ deployment/Azure/main.tf
@@ -0,0 +1,14 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
