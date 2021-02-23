ddee71e5a87c689f87fc3fd7592b3f2bec8a845e
AhmedMojahed <asaad0861@gmail.com>
2021-02-23 22:30:38 +0200

    azure: creating main resources

--- deployment/Azure/main.tf
+++ deployment/Azure/main.tf
@@ -12,3 +12,29 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+# Create Main resourses
+# resourse Group
+resource "azurerm_resource_group" "halangroup" {
+  name = "halangroup"
+  location = "West Europe"
+}
+# Networking
+# Main Virtual Network
+resource "azurerm_virtual_network" "halannetwork" {
+  name = "halannetwork"
+  location = azurerm_resource_group.halangroup.location
+  resource_group_name = azurerm_resource_group.halangroup.name
+  address_space = [ "10.0.0.0/16" ]
+}
+
+# Main Virtual Subnet
+resource "azurerm_subnet" "halansubnet" {
+  name = "sub1"
+  resource_group_name = azurerm_resource_group.halangroup.name
+  virtual_network_name = azurerm_virtual_network.halannetwork.name
+  address_prefixes = [ "10.0.0.0/24" ]
+}
+
+##########################################################
+
